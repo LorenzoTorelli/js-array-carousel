b7df700ff482e09b21819314d5d9ee1cc856e363
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-11-11 16:52:57 +0100

    Milestone-3

--- javaScript/script.js
+++ javaScript/script.js
@@ -29,65 +29,90 @@ const left = document.getElementById("left-side");
 let rightImg = '';
 let leftImg = '';
 
-for (i = 0; i < items.length; i++) {
+for (k = 0; k < items.length; k++) {
     
     leftImg += 
     `<div class = "l-item inactive" >
-        <img src="${items[i]}" alt = "slider">
+        <img src="${items[k]}" alt = "slider">
         <div class = "text" > 
-            <h3>${title[i]}</h3>
-            <p>${text[i]}</p>
+            <h3>${title[k]}</h3>
+            <p>${text[k]}</p>
         </div>
     </div> `;
 
     rightImg += 
     `<div class = "d-item" >
-        <img src="${items[i]}" alt = "slider">
-        
+        <img src="${items[k]}" alt = "slider">
     </div> `;
     
 }
 
-console.log(rightImg)
-
 left.innerHTML = leftImg;
 right.innerHTML = rightImg;
 
-
+// inizializzo le variabili
 let elements = document.getElementsByClassName("l-item");
-console.log(elements)
-elements[0].classList.remove("inactive");
-
+let elementsRight = document.getElementsByClassName("d-item");
 const up = document.getElementById("arrow-up");
 const down = document.getElementById("arrow-down");
 
+// i è un contatore 
 i = 0;
+
+// creo il caso base 
+elements[0].classList.remove("inactive");
+elementsRight[0].classList.add("d-selected");
+
 up.addEventListener("click", function() {
     if (i > 0) {
+
+        // immagini di sinistra 
         elements[i].classList.add("inactive");
         elements[i-1].classList.remove("inactive");
+
+        // immagini di destra 
+        elementsRight[i].classList.remove("d-selected");
+        elementsRight[i-1].classList.add("d-selected");
         i-=1
     }
     
     else if (i == 0) {
+
+        // immagini di sinistra 
         elements[i].classList.add("inactive");
-        elements[items.length].classList.remove("inactive");
-        i = items.length;
+        elements[items.length-1].classList.remove("inactive");
+
+        // immagini di destra 
+        elementsRight[i].classList.remove("d-selected");
+        elementsRight[items.length-1].classList.add("d-selected");
+        i = items.length-1;
     }
 
 }) 
 
 
 down.addEventListener("click", function() {
-    if (i < 4) {
+    if (i < items.length-1) {
+
+        // immagini di sinistra 
         elements[i].classList.add("inactive");
         elements[i+1].classList.remove("inactive");
+
+        // immagini di destra 
+        elementsRight[i].classList.remove("d-selected");
+        elementsRight[i+1].classList.add("d-selected");
         i+=1;
     }
 
-    else if (i == 4) {
+    else if (i == items.length-1) {
+        
+        // immagini di sinistra 
         elements[i].classList.add("inactive");
         elements[0].classList.remove("inactive");
+        
+        // immagini di destra 
+        elementsRight[i].classList.remove("d-selected");
+        elementsRight[0].classList.add("d-selected");
         i = 0;
     }
 
